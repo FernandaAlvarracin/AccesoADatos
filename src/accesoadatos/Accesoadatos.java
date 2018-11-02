c8ae5830d89ce665cc60e7c965e190c77fede0d5
Fernanda Alvarracin <fercruisecastle@gmail.com>
2018-11-02 17:28:04 -0300

    primer commit

---
+++ src/accesoadatos/Accesoadatos.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesoadatos;
+
+import accesoadatos.modelo.Alumno;
+import accesoadatos.modelo.AlumnoData;
+import accesoadatos.modelo.Conexion;
+import java.sql.SQLException;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ *
+ * @author programador
+ */
+public class Accesoadatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+  
+ //
+    Alumno alumno1 = new Alumno ("Juan Lopez", LocalDate.of(2010, Month.DECEMBER, 12), true);
+    Conexion con = new Conexion ();
+    AlumnoData ad = new AlumnoData (con);
+        
+    ad.guardarAlumno(alumno1);
+        
+    System.out.println("Nuevo alumnno id: "+ alumno1.getId());
+ //
+    }
+    
+    {Conexion con = new Conexion();
+        
+        AlumnoData alumnoData = new AlumnoData (con);
+        
+        Alumno alumno1 = new Alumno("Lopez Karina", LocalDate.of(1976,01, 20), true); 
+        
+        alumnoData.guardarAlumno(alumno1);
+        for(Alumno it: alumnoData.obtenerAlumnos()){
+            System.out.println("ID: "+it.getId());
+            System.out.println("Nombre: "+it.getNombre());
+        }
+
+     alumnoData.borrarAlumno(alumno1.getId());
+     
+     for(Alumno it: alumnoData.obtenerAlumnos()){
+            System.out.println("ID: "+it.getId());
+            System.out.println("Nombre: "+it.getNombre());
+    }
+     
+     alumnoData.actualizarAlumno(alumno1);
+               for(Alumno it:alumnoData.obtenerAlumnos()){
+                    System.out.println("ID: "+it.getId());
+            System.out.println("Nombre: "+it.getNombre());
+               }
+}
+}
---
+++ src/accesoadatos/modelo/Conexion.java
@@ -0,0 +1,44 @@
+
+package accesoadatos.modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author programador
+ */
+public class Conexion {
+              private String url="jdbc:mysql://localhost/prueba";
+              private String usuario="root";
+              private String password="";
+              private Connection con = null;
+public Conexion() {
+        
+        try {
+            //Cargamos driver en memoria
+            Class.forName("org.mariadb.jdbc.Driver");
+                                 
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }      
+       }
+       
+        public Connection getConexion() {
+                  try {
+                      if(con == null){
+                    con = DriverManager.getConnection
+                   ("url + \"?useLegacyDatetimeCode=false&serverTimezone=UTC\" + "
+                           + " \"&user=\" + usuario + \"&password=\" + password");
+                      }
+                  } catch (SQLException ex) {
+                      Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+                  }
+          return con;
+}
+    
+    
+}
